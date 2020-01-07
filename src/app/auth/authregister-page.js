065d820eeea66198abf096bb69a6f1fd92a02887
Vicky Hermawan <hv3344588@gmail.com>
2020-01-07 11:16:49 +0700

    register done

---
+++ src/app/auth/authregister-page.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import { Layout, BackTop } from 'antd';
+import { Route } from 'react-router-dom';
+import { API } from '../../common/api'
+import { connect } from 'react-redux';
+import { navigate } from '../../common/store/action'
+import RegsiterComponent from '../../modules/auth/component/authregister-component';
+
+import '../../assets/css/auth-login.css'
+
+class AuthRegister extends Component {
+    state = {
+        username: '',
+        email : '',
+        password: '',
+    }
+    componentDidMount(){
+        API.get(`/annual`)
+        .then((response) => {
+            console.log(response)
+        },(error) => {
+                console.log(error)
+            },
+        );
+    }
+
+    handleChange = (e) => {
+        let target = e.target.name;
+        let value = e.target.value;
+        this.setState({
+            [target]: value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault();
+        console.log('username : ', this.state.username)
+        console.log('email : ', this.state.email)
+        console.log('password : ', this.state.password)
+    }
+
+    render() {
+        return (
+        <RegsiterComponent
+            initialData={this.state}
+            navigate={this.props.navigate}
+            
+            handleChange={this.handleChange}
+            handleSubmit={this.handleSubmit}
+        />
+        );
+    }
+}
+ 
+
+const mapStateToProps = state => ({
+    
+});
+
+const mapDispatchToProps = (dispatch => ({
+    navigate,
+}))();
+
+const page = connect(mapStateToProps, mapDispatchToProps)(AuthRegister);
+export default page
--- src/common/router/router-config.js
+++ src/common/router/router-config.js
@@ -5,6 +5,7 @@ import Auth from '../layout/auth'
 import HomePage from '../../app/home/home-page'
 import EventPage from '../../app/event/event-page'
 import LoginComponent from '../../app/auth/authlogin-page'
+import RegisterComponent from '../../app/auth/authregister-page'
 
 const baseUrl = process.env.PUBLIC_URL;
 
@@ -12,6 +13,7 @@ const routeSources = [
     { component: HomePage, path: `${baseUrl}/`, exact: true },
     { component: EventPage, path: `${baseUrl}/event`, exact: true },
     { component: LoginComponent, path: `${baseUrl}/login`, exact: true },
+    { component: RegisterComponent, path: `${baseUrl}/register`, exact: true },
     // { component: ExamplePage, path: `${baseUrl}/example`, exact: true},
 ];
 
