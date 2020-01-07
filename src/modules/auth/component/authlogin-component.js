29b3d11d02c6892d3abd1c4768d1690160ae9326
Vicky Hermawan <hv3344588@gmail.com>
2020-01-07 10:11:21 +0700

    login done

--- src/modules/auth/component/authlogin-component.js
+++ src/modules/auth/component/authlogin-component.js
@@ -2,75 +2,78 @@ import React, { Component } from 'react';
 import { Layout, Row, Col, Button, Form,Icon, Input, } from 'antd';
 import '../../../assets/css/auth-login.css'
 // component
-import Navbar from '../../../common/layout/navbar-landing'
-import Footer from '../../../common/layout/footer-landing'
-import ButtonRounded from '../../../common/component/button/button-rounded'
+import InputAuth from '../../../common/component/input/input-auth'
+import ButtonAuth from '../../../common/component/button/button-auth'
 
 const { Content } = Layout;
+const logo = require(`../../../assets/images/logo.png`);
+const login = require(`../../../assets/images/login-image.png`);
 
 class LoginComponent extends Component{
     render(){
-        const image1 = require(`../../../assets/images/login-image.png`);
+        const { initialData, handleChange, handleSubmit } = this.props;
         return (
             <Layout className="login-container">
                  <Content style={{ overflow: "hidden" }}>
-                    <Row style={{minHeight: '100vh'}}>
-                        <Col lg={15} md={12} sm={12}>
+                    <Row>
+                        <Col lg={15} md={12} sm={12} className="background-white container-full">
                             <Row>
-                                <Col span={24}>
-                                    <div className="image-main-login">
-                                        <img
-                                            src={image1}
-                                            alt="Home 1"
-                                            style={{maxWidth: '100%'}}
-                                        />
+                                <div className="login-section-container">
+                                    <img src={logo} alt="EventIn logo" width="100"/>
+                                    <div className="auth-image-container">
+					                <img
+                                        src={login}
+                                        alt="EventIn login"
+                                        style={{maxWidth: '100%'}}
+                                    />
                                     </div>
-                                </Col>
-                                <Col span={24}>
-                                    <div className="container-login">
-                                        <span className="text-soft-blue title-login">Find a newst event nearby you </span>
-                                        <br/>
-                                        <span className="text-black description-login">Get ready to feel new sense</span>
-                                    </div>
-                                </Col>
+                                </div>
+                                <div className="text-soft-blue title-medium text-align-center">
+                                    Find a newst event nearby you
+                                </div>
+                                <div className="text-soft-black title-more-small text-align-center thin">
+                                    Get ready to fell new sense
+                                </div>
                             </Row>
                         </Col>
-                        <Col lg={9} md={12} sm={12}>
-                            <Row style={{backgroundColor:'#4D5AF2', paddingBottom:'100px'}}> 
-                                <Col span={24}>
-                                    <div className="container-form background-soft-blue">
-                                        <div className="inner-form">
-                                            <span className="text-white form-title">Welcome !</span>
-                                            <br/>
-                                            <span className="text-white form-description">Sign in to your account</span>
+                        <Col lg={9} md={12} sm={12} className="background-soft-blue container-full">
+                            <Row > 
+                                <Form onSubmit={handleSubmit}>
+                                    <div className="auth-form-container">
+                                        <div className="text-white text-align-center title-more-small mb-50 mt-60">
+                                            <p className="semi-bold m-0">Welcome!</p>
+                                            <p className="form-description">Sign in to your account</p>
+                                        </div>
+                                        <span className="auth-input-label text-white">Username</span>
+                                        <InputAuth
+                                            name='username'
+                                            placeholder="username"
+                                            onChange={handleChange}
+                                            value={initialData.username}
+                                            className="input-auth mt-5 mb-20"
+                                        />
+                                        <span className="auth-input-label text-white">Password</span>
+                                        <InputAuth
+                                            name='password'
+                                            placeholder="Password"
+                                            onChange={handleChange}
+                                            value={initialData.password}
+                                            className="input-auth mt-5"
+                                            iconType="lock"
+                                            type="password"
+                                        />
+                                         <p className="auth-reminder-label mt-10 text-white">Forgot Password?</p>
+                                        <div>
+                                            <ButtonAuth
+                                                text="Login"
+                                                className="auth-button-red mt-70 auth-button-login"
+                                                style={{borderRadius: '26px',backgroundColor:'#FA607E',border:'none',color:'#ffff'}}
+                                                block={true}
+                                            />
+                                             <p className="auth-login-label mt-10 text-align-center text-white">Don't have account? Register Now</p>
                                         </div>
                                     </div>
-                                </Col>
-                                <Col span={24}>
-                                    <div className="container-form background-soft-blue">
-                                            <Form layout="inline" >
-                                                <Input className="inputUser" 
-                                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                                    placeholder="Username"
-                                                />
-                                                 <Input className="inputPassword" 
-                                                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                                    placeholder="Password"
-                                                />
-                                            </Form>                                       
-                                    </div>
-                                    <div className="button-section-1-login">
-                                        <ButtonRounded
-                                            text="Login"
-                                            background="#FA607E"
-                                            textColor="#ffff"
-                                            border="1px solid #4D5AF2"
-                                            marginLeft={16}
-                                            className='button-login'
-                                            borderRadius={30}
-                                        />
-                                    </div>
-                                </Col>
+                                </Form>
                             </Row>
                         </Col>
                     </Row>
