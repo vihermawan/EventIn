f8f3efadf8cbd928fd73aa40339a3ae2c548deb4
Vicky Hermawan <hv3344588@gmail.com>
2020-01-12 13:42:26 +0700

    profile review

---
+++ src/app/profile/profile-page.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { navigate } from '../../common/store/action'
+import ProfileComponent from '../../modules/profile/component/profile-component';
+
+class ProfilePage extends Component {
+    state = {  }
+
+    render() { 
+        return ( 
+            <ProfileComponent
+                navigate={this.props.navigate}
+            />
+        );
+    }
+}
+ 
+const mapStateToProps = state => ({
+    
+});
+
+const mapDispatchToProps = (dispatch => ({
+    navigate,
+}))();
+
+const page = connect(mapStateToProps, mapDispatchToProps)(ProfilePage);
+export default page
