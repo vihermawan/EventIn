87b8236216e78815482d82a5bb0bdcebf65008a7
Vicky Hermawan <hv3344588@gmail.com>
2020-01-27 13:43:55 +0700

    login

--- src/common/initOptions.properties.js
+++ src/common/initOptions.properties.js
@@ -3,7 +3,7 @@
  * @since 28 Desember 2019
  */
 
-export const BASE_URL = 'http://localhost:8000';
+export const BASE_URL = 'http://localhost:8000/api';
 export const FAKE_URL = 'http://localhost:8000/api';
 export const CSRF_SERVICE_NAME = 'user/csrf-token';
 export const AUTHENTICATION_KEY = '';
