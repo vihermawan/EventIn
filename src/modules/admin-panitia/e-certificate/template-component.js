2995354a90e492cc7b8aac32fb421778a954be2f
Vicky Hermawan <hv3344588@gmail.com>
2020-02-28 16:53:57 +0700

    add tampilan template sertifikat

--- src/modules/admin-panitia/e-certificate/template-component.js
+++ src/modules/admin-panitia/e-certificate/template-component.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Layout, Breadcrumb, Row, Table, Input, Col,Tag } from 'antd';
+import { Layout, Breadcrumb, Row, Table, Icon, Col,Tag } from 'antd';
 import '../../../assets/css/dashboard-all/dashboard.css'
 import '../../../assets/css/dashboard-all/table-style.css'
 // component
-import TableProfile from '../../../common/component/table/table'
 import LoadingContainer from '../../../common/component/loading/loading-container'
+import ButtonDashboard from '../../../common/component/button/button-dashboard';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 // constant content
 const { Content } = Layout;
 
@@ -12,7 +13,8 @@ const { Content } = Layout;
 
 class TemplateCertificateComponent extends Component {
     render() { 
-      const { initialData, columns, data } = this.props
+      const { initialData } = this.props;
+      const image1 = require(`../../../assets/images/home-image1.png`);
         return ( 
             <Content
                 style={{
@@ -35,14 +37,73 @@ class TemplateCertificateComponent extends Component {
                                 <span>Template Certificate</span>
                             </div>
                             </Row>
-                            <LoadingContainer loading={initialData.loading}>
+                            <LoadingContainer loading={false}>
                                 <Row gutter={24} type="flex">
-                                    <TableProfile 
-                                        columns={columns} 
-                                        // dataSource={initialData.activeEvent} 
-                                        dataSource={data} 
-                                        className="table-active-event"
-                                    />
+                                    <Layout className="landing-container">
+                                        <Content style={{ overflow: "hidden" }}>
+                                        <Row style={{minHeight: '100%',marginBottom: '2%',marginTop:'2%'}}>
+                                                <Col lg={12} md={12} sm={12}>
+                                                    <Row>
+                                                        <Col span={24} >
+                                                            <div className="title-container-detail-event">
+                                                                <span className="detail-description">
+                                                                    Pada EventIn kami menyediakan template untuk sertifikat, template ini nantinya
+                                                                    dapat diunduh oleh panitia, template ini nantinya akan digunakan untuk melakukan tanda tangan digital
+                                                                    serta memberikan nama secara otomatis kepada sertif sesuai dengan nama peserta serta event yang sesuai.
+                                                                </span>
+                                                                <br/>
+                                                                <br/>
+                                                                <span className="detail-description">
+                                                                   Panitia yang sudah mengunduh template sertifikat, dapat mengubah desain yang ada di dalamnya. Namun tidak bisa
+                                                                   melakukan perubahan pada tata letak yang sudah ditentukan dari pihak EventIn. Sertifikat yang sudah selesai
+                                                                   di edit dapat diupload kembali yang nantinya akan dilakukan tanda tangan secara digital serta membuat nama secara
+                                                                   otomatis. Template Sertifikat dapat di unduh dengan menekan tombol unduh pada tombol di bawah ini !
+                                                                </span>
+                                                            </div>
+                                                        </Col>
+                                                        <Col span={24}>
+                                                            <div className="category-event">
+                                                                <Row>
+                                                                    <Col span={24} style={{ marginTop:'1%' }}>
+                                                                        <div>
+                                                                            <ButtonDashboard
+                                                                                text="Download"
+                                                                                height={20}
+                                                                                icon={faInfoCircle}
+                                                                                borderRadius="5px"
+                                                                                background="#4D5AF2"
+                                                                                // onClick={ }
+                                                                            />
+                                                                        </div>
+                                                                    </Col>
+                                                                    <Col span={24} style={{ marginTop:'4%' }}>
+                                                                        <div>
+                                                                            <p>More Info : </p>
+                                                                            <p className="text-soft-blue">
+                                                                                <Icon type="instagram" /> @EventIn
+                                                                            </p>
+                                                                            <p className="text-soft-blue">
+                                                                                <Icon type="ie" /> eventin@gmail.com
+                                                                            </p>
+                                                                        </div>
+                                                                    </Col>
+                                                                </Row>
+                                                            </div>
+                                                        </Col>
+                                                    </Row>
+                                                </Col>
+                                                <Col lg={12} md={12} sm={12}>
+                                                    <div className="image-big-container-detail">
+                                                        <img
+                                                            src={image1}
+                                                            alt="Home 1"
+                                                            style={{maxWidth: '100%'}}
+                                                        />
+                                                    </div>
+                                                </Col>
+                                            </Row>
+                                        </Content>
+                                    </Layout>
                                 </Row>
                             </LoadingContainer>
                         </div>
