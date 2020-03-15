c68fd72333287f78d7c5256f80feb5c491ff1438
Vicky Hermawan <hv3344588@gmail.com>
2020-03-15 15:40:28 +0700

    coba upload gambar

--- src/app/admin-panitia/dashboard/create-biodata-penandatangan-page.js
+++ src/app/admin-panitia/dashboard/create-biodata-penandatangan-page.js
@@ -13,7 +13,7 @@ class CreateBiodataPenandatanganPage extends Component {
        jabatan: '',
        instansi : '',
        nip : '',
-       profile_picture: '',
+       profile_picture: null,
        loading : false,
      }
 
@@ -27,8 +27,9 @@ class CreateBiodataPenandatanganPage extends Component {
 
     getBase64 = (img, callback)  =>{
         const reader = new FileReader();
-        reader.addEventListener('load', () => callback(reader.result));
         reader.readAsDataURL(img);
+        reader.addEventListener('load', () => callback(reader.result));
+       
     }
   
     beforeUpload = (file) => {
@@ -52,14 +53,44 @@ class CreateBiodataPenandatanganPage extends Component {
     }
         if (info.file.status === 'done') {
             // Get this url from response in real world.
-            this.getBase64(info.file.originFileObj, profile_picture =>
-            this.setState({
-                profile_picture,
-                loading: false,
-            }),
-        );
-    }
+            message.success(`${info.file.name} file uploaded successfully`);
+        //     this.getBase64(info.file.originFileObj, img =>
+        //         this.setState({
+        //             profile_picture : img,
+        //             loading: false,
+        //         }),
+        //  );
+        this.setState({
+            profile_picture : info.file.name,
+            loading: false,
+        })
+        }
     };
+
+    onChange = info => {
+        if (info.file.status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (info.file.status === 'done') {
+        
+          message.success(`${info.file.name} file uploaded successfully`);
+        //   this.setState({
+        //     profile_picture : info.file.name,
+        //     // loading: false,
+        //     })
+        this.uploadGambar(info);
+        } else if (info.file.status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+    }
+
+
+    uploadGambar = (event) => {
+        this.setState({
+            profile_picture:event.target.files[0]
+        })
+    }
+    
 
     handleSubmit = e => {
         e.preventDefault();
@@ -72,8 +103,12 @@ class CreateBiodataPenandatanganPage extends Component {
             profile_picture: this.state.profile_picture,   
         }
         console.log('params',params)
+        const fd = new FormData()
+        fd.append('profile_picture',this.state.profile_picture)
+        
+        console.log('fd',fd)
         // this.setState({loading: true})
-            API.post(`/panitia/create/biodata-penandatangan`, params)
+            API.post(`/panitia/create/biodata-penandatangan`, fd)
             .then(res => {
                 console.log('res',res)
                 // if(res.status == 201){
@@ -96,6 +131,8 @@ class CreateBiodataPenandatanganPage extends Component {
                 handleSubmit={this.handleSubmit}
                 beforeUpload = {this.beforeUpload}
                 handleChangeFoto = {this.handleChangeFoto}
+                uploadGambar={this.uploadGambar}
+                onChange = {this.onChange}
             />
         );
     }
