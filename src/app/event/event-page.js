6bcb86506d62111092dd41fa545bc79c3d076140
zainulmutaqin <zainulmutaqin15@gmail.com>
2020-03-18 14:15:53 +0700

    tes

--- src/app/event/event-page.js
+++ src/app/event/event-page.js
@@ -64,7 +64,7 @@ class EventPage extends Component {
         })
     }
 
-    //button detail participant
+    //button detail participant.
     onDetailEvent = (id) => {
         console.log('id ini',id)
         this.props.setIdEvent(id);
